c0625453208b160d96878f80f9bba55910fa32c8
Shen <73876759+Shenixreal@users.noreply.github.com>
2021-12-11 20:05:52 +0530

    Add files via upload

---
+++ story_generator.py
@@ -0,0 +1,16 @@
+#You can edit the story as you wish and add more characters, places and scenes.
+#Give a name for the prince, princess, and the kingdom, and a colour for the horse as inputs.
+#The story will be generated with your inputs.
+
+prince_name = str(input("Enter a name for the prince: "))
+princess_name = str(input("Enter a name for the princess: "))
+horse_colour = str(input("Enter a colour for the horse: "))
+kingdom_name = str(input("Enter a name for the kingdom: "))
+
+print("\n\n")
+print("Once upon a time in a far far away kingdom called " + kingdom_name + ", there lived a beautiful princess named " + princess_name + ".\n")
+print("One day, a handsome prince named " + prince_name+ " came to the kingdom of " + kingdom_name + " riding on a " + horse_colour + " horse.")
+print("He met princess " + princess_name + ", kissed her hand and asked her 'Will you marry me?'\n")
+print("Princess " + princess_name + " said 'No!'\n")
+print("So prince " + prince_name + " rode back to his kingdom and lived happily ever after.\n\n")
+print("--THE END!--\n")
